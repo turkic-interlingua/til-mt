b15c2dd087d54a6e66d96b2d5a423f783ea54852
Jamshidbek Mirzakhalov <mirzakhalov@Jamshidbeks-MBP.lan>
2021-12-05 23:35:23 -0500

    updated test data and readme

--- finetune/run_from_checkpoint.py
+++ finetune/run_from_checkpoint.py
@@ -6,8 +6,6 @@ model = TransformerModel.from_pretrained(
   data_name_or_path='data-bin/',
   bpe='sentencepiece',
   sentencepiece_model='spm/64k_bpe.model'
-  
-  
 )
 
 model.translate('<2en> Assalomu aleykum! Bu chatga xush kelibsiz!')
